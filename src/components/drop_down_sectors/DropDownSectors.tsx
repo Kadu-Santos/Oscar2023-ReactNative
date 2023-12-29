bbc0a23de16b4f8d914b1a2b5ae760ee6aa50ad6
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2023-12-29 13:36:31 -0300

    Add Components

--- src/components/drop_down_sectors/DropDownSectors.tsx
+++ src/components/drop_down_sectors/DropDownSectors.tsx
@@ -1,47 +1,52 @@
 import React, { useState } from 'react';
-import { View, Text} from 'react-native';
-import { styles } from "./styles";
-// import RNPickerSelect from 'react-native-picker-select';
-import { SelectList } from 'react-native-dropdown-select-list'
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+import { SelectList } from 'react-native-dropdown-select-list';
 
 const DropDownSectors = () => {
+  const [selectedOption, setSelectedOption] = useState<{ key: string; value: string } | null>(null);
 
-  const [selected, setSelected] = React.useState("");
 
   const dropdownOptions = [
-    { key:'1', value: 'Direção Geral' },
-    { key:'2', value: 'Coordenação de Gabinete' },
-    { key:'3', value: 'Coordenação de Gestão de Pessoas - CGP' },
-    { key:'4', value: 'Departamento Financeiro - Compras' },
-    { key:'5', value: 'Direção de Ensino' },
-    { key:'6', value: 'Diretoria de Pesquisa' },
-    { key:'7', value: 'Diretoria de Extensão' },
-    { key:'8', value: 'Docentes' },
-    { key:'9', value: 'Setor de Estágio' },
-    { key:'10', value: 'Núcleo Pedagógico' },
-    { key:'12', value: 'CGAE' },
-    { key:'12', value: 'Almoxarifado' },
-    { key:'13', value: 'Biblioteca' },
-    { key:'14', value: 'Secretaria' },
-    { key:'15', value: 'Gestão de Tecnologia da Informação - CGTI' },
-    { key:'16', value: 'Segurança - Vigilância' },
-    { key:'17', value: 'Lavanderia' },
-    { key:'18', value: 'Fadetec' },
-    { key:'19', value: 'Outro: Qual?' },
+    { key: '1', value: 'Direção Geral' },
+    { key: '2', value: 'Coordenação de Gabinete' },
+    { key: '3', value: 'Coordenação de Gestão de Pessoas - CGP' },
+    { key: '4', value: 'Departamento Financeiro - Compras' },
+    { key: '5', value: 'Direção de Ensino' },
+    { key: '6', value: 'Diretoria de Pesquisa' },
+    { key: '7', value: 'Diretoria de Extensão' },
+    { key: '8', value: 'Docentes' },
+    { key: '9', value: 'Setor de Estágio' },
+    { key: '10', value: 'Núcleo Pedagógico' },
+    { key: '12', value: 'CGAE' },
+    { key: '12', value: 'Almoxarifado' },
+    { key: '13', value: 'Biblioteca' },
+    { key: '14', value: 'Secretaria' },
+    { key: '15', value: 'Gestão de Tecnologia da Informação - CGTI' },
+    { key: '16', value: 'Segurança - Vigilância' },
+    { key: '17', value: 'Lavanderia' },
+    { key: '18', value: 'Fadetec' },
+    { key: '19', value: 'Outro: Qual?' },
   ];
+
+  const handleDropdownChange = (value: { key: string; value: string } | null) => {
+    setSelectedOption(value);
+    let selectedSetor = value;
+    console.log('Opção selecionada:', selectedSetor);
+  };
   
 
   return (
-
-
     <View style={styles.container}>
-      <SelectList 
-      setSelected={(val: React.SetStateAction<string>) => setSelected(val)} 
-      data={dropdownOptions} 
-      save="value"
+      <SelectList
+        setSelected={(val: { key: string; value: string; } | null) => handleDropdownChange(val)}
+        data={dropdownOptions}
+        save="value"
+        searchPlaceholder="Pesquisar"
+        placeholder="Escolha uma Opção"
       />
-    </View>
 
+    </View>
   );
 };
 
