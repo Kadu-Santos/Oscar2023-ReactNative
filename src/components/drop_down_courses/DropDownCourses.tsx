e92811a394bf99b5fd3ad647e85c0a32dd8f3eba
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-01-04 14:55:37 -0300

    updating components

--- src/components/drop_down_courses/DropDownCourses.tsx
+++ src/components/drop_down_courses/DropDownCourses.tsx
@@ -2,27 +2,12 @@ import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { styles } from './styles';
 import { SelectList } from 'react-native-dropdown-select-list';
+import dropdownCoursesOptions from './dropdownCoursesOptions';
 
 
 const DropDownCourses = () => {
   const [selectedOption, setSelectedOption] = useState<{ key: string; value: string } | null>(null);
-  
 
-  const dropdownOptions = [
-    { key: '1', value: 'Licenciatura em Ciências Biológicas' },
-    { key: '2', value: 'Licenciatura em Matemática' },
-    { key: '3', value: 'Licenciatura em Química' },
-    { key: '4', value: 'Licenciatura em Física' },
-    { key: '5', value: 'Licenciatura em Pedagogia' },
-    { key: '6', value: 'Bacharelado em Medicina Veterinária' },
-    { key: '7', value: 'Bacharelado em Engenharia Florestal' },
-    { key: '8', value: 'Bacharelado em Engenharia de Alimentos' },
-    { key: '9', value: 'Bacharelado em Sistemas de Informação' },
-    { key: '10', value: 'Técnico em Informática' },
-    { key: '11', value: 'Técnico em Agropecuária' },
-    { key: '12', value: 'Técnico em Agroindústria' },
-    { key: '13', value: 'Outro: Qual?' },
-  ];
 
   const handleDropdownChange = (value: { key: string; value: string } | null) => {
     setSelectedOption(value);
@@ -35,7 +20,7 @@ const DropDownCourses = () => {
     <View style={styles.container}>
       <SelectList
         setSelected={(val: { key: string; value: string; } | null) => handleDropdownChange(val)}
-        data={dropdownOptions}
+        data={dropdownCoursesOptions}
         save="value"
         searchPlaceholder="Pesquisar"
         placeholder="Escolha uma Opção"
@@ -44,5 +29,3 @@ const DropDownCourses = () => {
     </View>
   );
 };
-
-export default DropDownCourses;
---
+++ src/components/drop_down_courses/dropdownCoursesOptions.ts
@@ -0,0 +1,17 @@
+const dropdownCoursesOptions = [
+  { key: '1', value: 'Licenciatura em Ciências Biológicas' },
+  { key: '2', value: 'Licenciatura em Matemática' },
+  { key: '3', value: 'Licenciatura em Química' },
+  { key: '4', value: 'Licenciatura em Física' },
+  { key: '5', value: 'Licenciatura em Pedagogia' },
+  { key: '6', value: 'Bacharelado em Medicina Veterinária' },
+  { key: '7', value: 'Bacharelado em Engenharia Florestal' },
+  { key: '8', value: 'Bacharelado em Engenharia de Alimentos' },
+  { key: '9', value: 'Bacharelado em Sistemas de Informação' },
+  { key: '10', value: 'Técnico em Informática' },
+  { key: '11', value: 'Técnico em Agropecuária' },
+  { key: '12', value: 'Técnico em Agroindústria' },
+  { key: '13', value: 'Outro: Qual?' },
+];
+
+export default dropdownCoursesOptions;
