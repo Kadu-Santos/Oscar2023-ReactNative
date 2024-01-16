1a31c8755e76d313b523103cfa5e3a0d742b6fb6
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-01-16 17:45:50 -0300

    add back button ScreenQuestion

--- src/app/(tabs)/Question/ScreenQuestion.tsx
+++ src/app/(tabs)/Question/ScreenQuestion.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
 import { styles } from './styles';
 import ImageQuestion from '../../../components/image_question/ImageQuestion';
 import Question from '../../../components/question/Question';
@@ -7,11 +7,20 @@ import Options from '../../../components/options/Options';
 import { Link } from 'expo-router';
 import FristOptionsRoute from "./FristOptionsRoute";
 import SecondOptionsRoute from "./SecondOptionsRoute";
+import { TouchableOpacity } from 'react-native';
+
 export default function ScreenQuestion() {
   const [QuestionID, setQuestionID] = useState('1');
   const [ButtonID, setButtonID] = useState('');
-  
+  const [VoltarVisibility, setVoltarVisibility] = useState(false);
 
+  const [returnroutes, setreturnroutes] = useState<string[]>([]);
+
+  
+  useEffect(() => {
+    setVoltarVisibility(QuestionID !== '1');
+  }, [QuestionID]);
+  
   const getFristOptionsRouteByKey = (key: string | undefined) => {
     const FristOptionsRouteEntry = FristOptionsRoute.find((entry) => entry.key === key);
     return FristOptionsRouteEntry ? FristOptionsRouteEntry.value : '';
@@ -23,25 +32,48 @@ export default function ScreenQuestion() {
   };
 
   const handlePress = (newValue: string, buttonPressed: number) => {
-    setButtonID(buttonPressed === 1? '1': '2');
+    setButtonID(buttonPressed === 1 ? '1' : '2');
     
     if (newValue === '0') {
-        if (buttonPressed === 1) {
-            const FristOP = getFristOptionsRouteByKey(QuestionID);
-            
-        }else{
-            const SecondOP = getSecondOptionsRouteByKey(QuestionID);
-            
-        }
+      if (buttonPressed === 1) {
+        const FristOP = getFristOptionsRouteByKey(QuestionID);
+      } else {
+        const SecondOP = getSecondOptionsRouteByKey(QuestionID);
+      }
     } else {
+      const previouskey: string = QuestionID;
+      setreturnroutes([...returnroutes, previouskey]);
       setQuestionID(newValue);
     }
   };
 
-
+  const actionVoltar = () => {
+    if (returnroutes.length > 0) {
+      const ultimoElemento = returnroutes.slice(-1)[0];
+      setQuestionID(ultimoElemento);
+      const novoArray = returnroutes.slice(0, -1);
+      setreturnroutes(novoArray);
+    }
+  }
+  
   return (
 
       <View style={styles.container}>
+
+        { VoltarVisibility &&
+            <View style={styles.containerReturn}>
+              <TouchableOpacity 
+                style={styles.return} 
+                onPress={actionVoltar}
+              >
+    
+                <Text style={styles.returnTxt}>
+                  Voltar
+                </Text>
+    
+              </TouchableOpacity>
+            </View>
+          }
 
         <ImageQuestion ImageKey={QuestionID} />
 
--- src/app/(tabs)/Question/styles.ts
+++ src/app/(tabs)/Question/styles.ts
@@ -25,5 +25,28 @@ export const styles = StyleSheet.create({
     textAlign: 'center',
   },
 
+  containerReturn: {
+    
+  },
+
+  return: {
+    width: '30%',
+    marginTop: '3%',
+    marginLeft: '3%',
+    borderRadius: 15,
+    alignItems: "center",
+    padding: '1%',
+    backgroundColor: '#d6d6d654',
+    borderStyle:"solid",
+    marginBottom: 0,
+    opacity: 0.8,
+  },
+
+  returnTxt: {
+    color: '#000',
+    fontSize: 17,
+
+  },
+
 
 });
